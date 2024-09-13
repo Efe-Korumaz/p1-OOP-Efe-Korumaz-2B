2ec2bfb87d8e666069cc890dbcc1aa5a2502af42
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-13 14:52:17 +0200

    ocm

---
+++ opdracht2.1.php
@@ -0,0 +1,14 @@
+<?php
+$age = 19;
+
+if ($age > 18) {
+    echo 'Je bent volwassen';
+} elseif ($age > 4) {
+    echo "je bent een kind";
+} else {
+    echo 'je bent een baby of peuter';
+}
+
+
+
+
