339eacf248da1172626c18482e634fb387d571ec
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-13 14:46:10 +0200

    com

---
+++ opdracht1.4.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 3.14;
+$c = 'PHP';
+
+$a = 'tien';
+$b = 3;
+$c = false;
+
+echo $a;
+echo '<br>';
+echo $b;
+echo '<br>';
+echo $c;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+?>
