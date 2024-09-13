da4275e995a9a7cb47c0876aae268b4eea42658f
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-13 13:17:06 +0200

    commit

---
+++ opdracht1.php
@@ -0,0 +1,14 @@
+<?php
+$firstName = 'Jan';
+$age = 25;
+$height = 1.75;
+$isStudent = true;
+
+echo $firstName;
+echo '<br>';
+echo $age;
+echo '<br>';
+echo $height;
+echo '<br>';
+echo $isStudent;
+?>
