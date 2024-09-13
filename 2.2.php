9115dac35a6dce7a5d8b2ebd9ab3f1c2861384bf
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-13 14:56:07 +0200

    commit

---
+++ 2.2.php
@@ -0,0 +1,12 @@
+<?php
+$score = 56;
+
+if ($score >= 90) {
+    echo 'you get an A';
+} elseif ($score >= 80) {
+    echo 'you get a B';
+} elseif ($score >= 70) {
+    echo 'you get a c';
+} else {
+    echo 'you get a F';
+}
