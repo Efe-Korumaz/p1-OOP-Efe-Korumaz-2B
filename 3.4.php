aee341499ec8c69cb40116dcaad36f5b4cb15d34
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-13 16:00:56 +0200

    d

---
+++ 3.4.php
@@ -0,0 +1,11 @@
+<?php
+$student = ["name" => "Laptop", "price" => 899.99, "InStock" => true];
+$student['brand'] = 'HP';
+$student['price'] = 799.99;
+echo $student['name'];
+echo '<br>';
+echo $student['price'];
+echo '<br>';
+echo $student['InStock'];
+echo '<br>';
+echo $student['brand'];
