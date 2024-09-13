c3d252cd3f1508003e2b50e5866eb621fdc76e01
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-13 14:34:36 +0200

    commt

--- opdracht1.3.php
+++ opdracht1.3.php
@@ -3,4 +3,11 @@ $city = 'Amsterdam';
 $population = 821752;
 $averageTemperature = 17.5;
 $isCapital = true;
-    ?>
+echo 'name of city is' . ' ' . $city;
+echo '<br>';
+echo 'the population is' . ' ' . $population;
+echo '<br>';
+echo 'the temp is' . ' ' . $averageTemperature;
+echo '<br>';
+echo 'is it the capital?' . ' ' . $isCapital;
+?>
