aa981506d409b6a93ce0468d35dc682161f08aa0
Efe-Korumaz <9022358@student.Zadkine.nl>
2024-09-16 13:45:09 +0200

    commit

---
+++ 6.1.php
@@ -0,0 +1,9 @@
+<?php
+
+$numbers = ['5', '10', '15', '20', '25'];
+
+for ($i = 0; $i < count($numbers); $i++) {
+
+    echo "Het orginele getal is: " . $numbers[$i] . "<br>";
+    echo "Vermedivuldigd met 2 is het: " . $numbers[$i] * 2 . "<br>";
+}
